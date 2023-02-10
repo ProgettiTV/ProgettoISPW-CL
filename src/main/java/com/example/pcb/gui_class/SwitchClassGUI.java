687ed89197c4cf012c69428a803e4dfb7e6b8b86
giustiniman <justointer@gmail.com>
2023-02-10 11:04:15 +0100

    change graphic

--- src/main/java/com/example/pcb/gui_class/SwitchClassGUI.java
+++ src/main/java/com/example/pcb/gui_class/SwitchClassGUI.java
@@ -2,12 +2,10 @@ package com.example.pcb.gui_class;
 
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.util.Objects;
